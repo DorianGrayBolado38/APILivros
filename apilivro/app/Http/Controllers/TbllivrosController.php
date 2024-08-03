e17ee52fa2fb6eea16f1d59131385cc303d041ba
DorianGrayBolado38 <lukinhasdeoliveiramoraes@gmail.com>
2024-08-02 22:50:07 -0300

    funções da api

---
+++ apilivro/app/Http/Controllers/TbllivrosController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TbllivrosController extends Controller
+{
+    //costruir o crud.
+
+
+    //Mostrar todos os registros da tabela livros
+    //Crud -> Read(leitura) Select/Visualizar
+
+    public function index(){
+
+    }
+    public function store(){
+
+    }
+    public function show(){
+
+    }
+    public function update(){
+
+    }
+    public function destroy(){
+        
+    }
+}
